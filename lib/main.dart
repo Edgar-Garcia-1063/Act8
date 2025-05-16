50849d88f48cdca33ca6429e474328f74e72f233
EDGAR_AXEL_GARCIA_AGUIRRE <a.22308051281063@cbtis128.edu.mx>
2025-05-16 15:19:33 +0000

    a

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,127 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: LoginPage(),
+  ));
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+      body: Column(
+        children: [
+          // Parte superior decorada
+          Container(
+            width: double.infinity,
+            height: 320,
+            decoration: const BoxDecoration(
+              color: Colors.red,
+              borderRadius: BorderRadius.only(
+                bottomRight: Radius.circular(80),
+              ),
+            ),
+            child: Stack(
+              children: const [
+                Positioned(
+                  top: 60,
+                  left: 40,
+                  child: Icon(Icons.lightbulb_outline,
+                      size: 40, color: Colors.white),
+                ),
+                Positioned(
+                  top: 40,
+                  right: 80,
+                  child: Icon(Icons.light_rounded,
+                      size: 40, color: Colors.white),
+                ),
+                Positioned(
+                  top: 30,
+                  right: 30,
+                  child:
+                      Icon(Icons.access_time, size: 30, color: Colors.white),
+                ),
+                Center(
+                  child: Text(
+                    'Login',
+                    style: TextStyle(
+                      fontSize: 36,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+
+          const SizedBox(height: 40),
+
+          // Inputs
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+            child: Column(
+              children: [
+                _inputField("Email or Phone number"),
+                const SizedBox(height: 20),
+                _inputField("Password", isPassword: true),
+                const SizedBox(height: 30),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.red,
+                      padding: const EdgeInsets.symmetric(vertical: 16),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: const Text(
+                      "Login",
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                  "Forgot Password?",
+                  style: TextStyle(
+                    color: Colors.grey,
+                    decoration: TextDecoration.underline,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+    );
+  }
+
+  Widget _inputField(String hint, {bool isPassword = false}) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(14),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.red.shade100,
+            blurRadius: 10,
+            offset: const Offset(0, 6),
+          ),
+        ],
+      ),
+      child: TextField(
+        obscureText: isPassword,
+        decoration: InputDecoration(
+          border: InputBorder.none,
+          hintText: hint,
         ),
       ),
     );
